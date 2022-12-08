7e4c4fc0e41db48e3c82a25880ef09311290b3c6
pavan <awsdevops.pavan@gmail.com>
2022-12-08 16:17:51 +0530

    commented dynanoDB code and delete it from tf state list

--- Terraform/resources_dynamo_db.tf
+++ Terraform/resources_dynamo_db.tf
@@ -1,9 +1,12 @@
-resource "aws_dynamodb_table" "state_locking" {
-  hash_key = "LockID"
-  name     = "dynamodb-state-locking"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-  billing_mode = "PAY_PER_REQUEST"
-}
+# resource "aws_dynamodb_table" "state_locking" {
+#   hash_key = "LockID"
+#   name     = "dynamodb-state-locking"
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+#   billing_mode = "PAY_PER_REQUEST"
+# }
+
+# commented this code and removed from state file list because as it is created and we no need to make
+# dynamodb under terraform control
