0d2114241d893971314dfb871cac116cab9453e6
pavan <awsdevops.pavan@gmail.com>
2022-12-08 15:53:27 +0530

    Added dynamoDB state locking in backend.tf

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -2,5 +2,8 @@ terraform {
   backend "s3" {
     bucket = "hyderabad-s3"
     key    = "devopsb27.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "dynamodb-state-locking"
+    encrypt        = true
   }
 }
