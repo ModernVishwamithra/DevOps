8cc1ed0ab47e704088571c438fb6b779a2bba9a5
pavan <awsdevops.pavan@gmail.com>
2023-01-03 21:23:25 +0530

    Deleted dev branch

--- Terraform/Terraform_Modules/Dev/providers.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_version = "1.3.6" # this is the terraform version to be used , we are forcing to use this
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0" # this is the aws version to be used 
-    }
-  }
-}
