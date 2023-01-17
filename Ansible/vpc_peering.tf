a383657c113b7ebfc3d2e7b808f535c3cfba36b2
pavan <awsdevops.pavan@gmail.com>
2023-01-17 16:12:18 +0530

    Added vpc peering code

--- Ansible/vpc_peering.tf
+++ Ansible/vpc_peering.tf
@@ -1,3 +1,17 @@
 data "aws_vpc" "ansible_controller_vpc" {
     id = "vpc-0d15fdcea83ad6549"
 }
+
+resource "aws_vpc_peering_connection" "ansible_vpc_peering" {
+  peer_vpc_id   = aws_vpc.default.id
+  vpc_id        = aws_vpc.ansible_controller_vpc.id
+  auto_accept = true
+
+#   accepter {
+#     allow_remote_vpc_dns_resolution = true
+#   }
+
+#   requester {
+#     allow_remote_vpc_dns_resolution = true
+#   }
+}
